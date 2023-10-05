d8560e837720c065b2c6d5c7d40ba4440e2d9e12
OleksandrVaniuta <vaniutasa@gmail.com>
2023-10-06 00:12:02 +0300

    Updates

---
+++ static/js/318.c814de45.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinforce_test_task=self.webpackChunkinforce_test_task||[]).push([[318],{7318:function(e,t,n){n.r(t);var s=n(184);t.default=function(){return(0,s.jsx)("div",{children:(0,s.jsx)("h1",{children:"ProductPage"})})}}}]);
+//# sourceMappingURL=318.c814de45.chunk.js.map
