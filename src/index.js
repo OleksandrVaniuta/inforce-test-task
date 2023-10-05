73eb6c6f334e7cd81b494f99b6d499caebe0f85b
OleksandrVaniuta <vaniutasa@gmail.com>
2023-10-06 00:08:12 +0300

    gh pages, change name

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { theme } from './theme/theme';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="">
+      <BrowserRouter basename="inforce-test-task">
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
